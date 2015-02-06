97a3c35e260bd566b3e83849321b874e8dcdf082
rkprater <rkprater@icloud.com>
2015-02-05 23:52:10 -0500

    added plugin

---
+++ main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+  // Type.js main content
+  $(function(){
+    $(".element").typed({
+      strings: [ "",
+        " say what is new",
+        " say what is fun",
+        " say what is for you",
+        " say what is for friends",
+        " try say what today"],
+        typeSpeed: 40,
+        backDelay: 900,
+        loop: true,
+        loopCount: false,
+        });
+    });
+
+});
